5a87f2eb2a5a7755109e3f795e4763de731d00bf
Petya Petrova <147195169+petyap04@users.noreply.github.com>
2024-11-04 10:51:45 +0200

    Delete Week2 directory

--- Week2/bg/sofia/uni/fmi/mjt/gameplatform/store/item/filter/ItemFilter.java
+++
@@ -1,6 +0,0 @@
-package bg.sofia.uni.fmi.mjt.gameplatform.store.item.filter;
-import bg.sofia.uni.fmi.mjt.gameplatform.store.item.StoreItem;
-
-public interface ItemFilter {
-    boolean matches(StoreItem item);
-}
