f92b4be9263b353b49dc9bd17697a86b1dc3f2b5
IEMbg <ivan_em_makaveev@abv.bg>
2024-12-22 23:14:54 +0200

    Modified book parsing

--- bg/sofia/uni/fmi/mjt/goodreads/book/Book.java
+++ bg/sofia/uni/fmi/mjt/goodreads/book/Book.java
@@ -1,57 +1,48 @@
 package bg.sofia.uni.fmi.mjt.goodreads.book;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public record Book(
-        String ID,
-        String title,
-        String author,
-        String description,
-        List<String> genres,
-        double rating,
-        int ratingCount,
-        String URL
+    String ID,
+    String title,
+    String author,
+    String description,
+    List<String> genres,
+    double rating,
+    int ratingCount,
+    String URL
 ) {
+    private static final String GENRES_LIST_START = "[";
+    private static final String GENRES_LIST_END = "]";
+    private static final String GENRE_CONTENT_MARKER = "'";
+    private static final String DATA_SEPARATOR = ",";
 
-    private static String getBegin(String begin) {
-        if (begin == null) {
-            throw new IllegalArgumentException("The string can't be null!");
-        }
-        if (begin.startsWith("\"[")) {
-            StringBuilder result = new StringBuilder(begin);
-            result.delete(0,2);
-            return result.toString();
-        }
-        throw new IllegalArgumentException("Invalid beginning!");
+    private static String parseGenresStart(String begin) {
+        StringBuilder result = new StringBuilder(begin);
+        result.delete(0, GENRES_LIST_START.length());
+        return result.toString();
     }
 
-    private static String getEnd(String end) {
-        if (end == null) {
-            throw new IllegalArgumentException("The string can't be null!");
-        }
-        if (end.endsWith("]\"")) {
-            StringBuilder result = new StringBuilder(end);
-            result.delete(result.length() - 2, result.length());
-            return result.toString();
-        }
-        throw new IllegalArgumentException("Invalid ending!");
+    private static String parseGenresEnd(String end) {
+        StringBuilder result = new StringBuilder(end);
+        result.delete(result.length() - GENRES_LIST_END.length(), result.length());
+        return result.toString();
     }
 
     private static List<String> getGenres(String str) {
-        if (str == null) {
-            throw new IllegalArgumentException("Genres can't be null!");
+        if (!str.startsWith(GENRES_LIST_START) && !str.endsWith(GENRES_LIST_END)) {
+            throw new IllegalArgumentException("The genre string is not in the right format");
         }
 
-        if (!str.startsWith("\"[") && !str.endsWith("]\"")) {
-            throw new IllegalArgumentException("The string is not in the right format");
-        }
-        String[] tokensOfGenres = str.split(",");
-        tokensOfGenres[0] = getBegin(tokensOfGenres[0]);
-        tokensOfGenres[tokensOfGenres.length - 1] = getEnd(tokensOfGenres[tokensOfGenres.length - 1]);
+        String[] tokensOfGenres = str.split(DATA_SEPARATOR);
+        tokensOfGenres[0] = parseGenresStart(tokensOfGenres[0]);
+        tokensOfGenres[tokensOfGenres.length - 1] = parseGenresEnd(tokensOfGenres[tokensOfGenres.length - 1]);
         List<String> genres = new ArrayList<>();
         for (String curr : tokensOfGenres) {
-            String trimmedGenre = curr.replace("'", "").trim();
+            String trimmedGenre = curr.replace(GENRE_CONTENT_MARKER, "").trim();
             if (!trimmedGenre.isEmpty()) {
                 genres.add(trimmedGenre);
             }
@@ -59,43 +50,33 @@ public record Book(
         return genres;
     }
 
-    private static String getDescription(String string) {
-        if (string == null) {
-            throw new IllegalArgumentException("The string can't be null!");
-        }
-        if (!string.startsWith("\"") || !string.endsWith("\"")) {
-            throw new IllegalArgumentException("The string is not in the right format!");
-        }
-        StringBuilder result = new StringBuilder(string);
-        result.deleteCharAt(0);
-        result.deleteCharAt(result.length() - 1);
-
-        int index = result.indexOf("\"\"\"");
-        while (index != -1) {
-            result.replace(index, index + 3, "\"");
-            index = result.indexOf("\"\"\"", index + 1);
-        }
-        return result.toString();
+    private static int getRatingCount(String str) {
+        return Integer.parseInt(str.replace(DATA_SEPARATOR, ""));
     }
 
     public static Book of(String[] tokens) {
-
-        if (tokens == null) {
-            throw new IllegalArgumentException("The tokens are null!");
+        if (tokens == null || Arrays.stream(tokens).anyMatch(Objects::isNull)) {
+            throw new IllegalArgumentException("Cannot parse null tokens!");
         }
 
+        if (tokens.length != BookParseIndex.values().length) {
+            throw new IllegalArgumentException("Incorrect book tokens count!");
+        }
+
+        tokens = Arrays.stream(tokens).map(String::trim).toArray(String[]::new);
+
         try {
-            String id = tokens[0].trim();
-            String title = tokens[1].trim();
-            String author = tokens[2].trim();
-            String description = getDescription(tokens[3].trim());
-            List<String> genres = getGenres(tokens[4].trim());
-            double rating = Double.parseDouble(tokens[5].trim());
-            int ratingCount = Integer.parseInt(tokens[6].trim());
-            String URL = tokens[7].trim();
-            return new Book(id, title, author, description, genres, rating, ratingCount, URL);
-        } catch (Throwable e) {
-            throw new IllegalArgumentException("Failed to parsing for Book", e);
+            String id = tokens[BookParseIndex.ID.getId()];
+            String title = tokens[BookParseIndex.TITLE.getId()];
+            String author = tokens[BookParseIndex.AUTHOR.getId()];
+            String description = tokens[BookParseIndex.DESCRIPTION.getId()];
+            List<String> genres = getGenres(tokens[BookParseIndex.GENRES.getId()]);
+            double rating = Double.parseDouble(tokens[BookParseIndex.RATING.getId()]);
+            int ratingCount = getRatingCount(tokens[BookParseIndex.RATING_COUNT.getId()]);
+            String url = tokens[BookParseIndex.URL.getId()];
+            return new Book(id, title, author, description, genres, rating, ratingCount, url);
+        } catch (Exception e) {
+            throw new RuntimeException("Book data parsing failed!", e);
         }
     }
 }
---
+++ bg/sofia/uni/fmi/mjt/goodreads/book/BookParseIndex.java
@@ -0,0 +1,22 @@
+package bg.sofia.uni.fmi.mjt.goodreads.book;
+
+public enum BookParseIndex {
+    ID(0),
+    TITLE(1),
+    AUTHOR(2),
+    DESCRIPTION(3),
+    GENRES(4),
+    RATING(5),
+    RATING_COUNT(6),
+    URL(7);
+
+    private final int id;
+
+    BookParseIndex(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+}
